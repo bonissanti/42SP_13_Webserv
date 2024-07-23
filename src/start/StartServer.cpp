e6e20a3483ff39e7e4657b2dfaabade20932de0e
bonissanti <brunoonissanti@gmail.com>
2024-07-23 11:51:42 -0300

    feat: startServer working in progress

--- src/start/StartServer.cpp
+++ src/start/StartServer.cpp
@@ -6,7 +6,7 @@
 /*   By: brunrodr <brunrodr@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 18:21:01 by brunrodr          #+#    #+#             */
-/*   Updated: 2024/07/22 20:01:17 by brunrodr         ###   ########.fr       */
+/*   Updated: 2024/07/23 11:46:08 by brunrodr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,10 @@ void	Server::startServer(std::vector<Server> servers)
 {
 	struct sockaddr_in serverAddr;
 
+	bzero(&serverAddr, sizeof(serverAddr));
 	serverAddr.sin_family = AF_INET;
 	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverAddr.sin_port = htons(servers[0]._listen);
 	
-	// for ... servers
-	// serverAddr.sin_port = htons(servers[]._port);
-	// socket
-	//setsockopt
+	
 }
