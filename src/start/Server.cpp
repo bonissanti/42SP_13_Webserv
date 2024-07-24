6eebacf9869d94a6652ca59d165bbc51c97d15db
Renan Batista <rbatista@resutec.com.br>
2024-07-23 21:25:48 -0300

    feat(server): Adicionado campos de rota e erro. Adicionado MOVED_PERMANENTLY aos erros para tratar redirect de server para novas paginas. Adicionado _return, _autoindex, _route, _redirect e alterado nome dos campos adicionado _ na struct. Alterado cgi para boleano no server.

--- src/start/Server.cpp
+++ src/start/Server.cpp
@@ -1,29 +1,18 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Server.cpp                                         :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: brunrodr <brunrodr@student.42sp.org.br>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/18 12:21:41 by brunrodr          #+#    #+#             */
-/*   Updated: 2024/07/29 18:13:35 by brunrodr         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../../include/Server.hpp"
+
 #include "../../include/Minilib.hpp"
 
 static int getKeyIndex(std::string key);
 
 Server::Server()
 {
-    t_error_page status = DEFAULT;
+    status_request status = DEFAULT;
 
     _listen = 8080;
     _client_max_body_size = 30 * MB;
     _server_name = "default";
     _host = "localhost";
-    _root = "/data/"; // = localhost/image.png -> /data/image.png
+    _root = "/data/";  // = localhost/image.png -> /data/image.png
 
     _error_page.push_back(status);
 }
@@ -34,11 +23,11 @@ t_route Server::createRoute()
 {
     t_route route;
 
-    route.autoindex = false;
-    route.root = "/data/"; // se não especificado, fica o mesmo do server
-    route.allow_methods = "GET POST DELETE";
-    route.index = "index.html";
-    route.cgi = ""; /* na hora de usar cgi, olhamos se ele é vazio, se não for, concatenamos 
+    route._autoindex = false;
+    route._root = "/data/";  // se não especificado, fica o mesmo do server
+    route._allow_methods = "GET POST DELETE";
+    route._index = "index.html";
+    route._cgi = ""; /* na hora de usar cgi, olhamos se ele é vazio, se não for, concatenamos 
     CGIDIR + route.cgi (/cgi/arquivo.py) */
     return (route);
 }
@@ -56,51 +45,51 @@ std::vector<Server> Server::creatingServers(int numServers, std::vector<std::str
     int serverIndex = -1;
     std::vector<Server> servers(numServers);
 
-	for (size_t i = 0; i < lines.size(); i++) {
-		std::string       key, value;
-		std::stringstream ss(lines[i]);
+    for (size_t i = 0; i < lines.size(); i++) {
+        std::string key, value;
+        std::stringstream ss(lines[i]);
 
-        if (lines[i].substr(0, 6) == "server"){
+        if (lines[i].substr(0, 6) == "server") {
             serverIndex++;
             continue;
         }
-		if (getline(ss, key, '=') && getline(ss, value) && serverIndex >= 0 && serverIndex < numServers) 
-		{
-		    switch (getKeyIndex(trim(key)))
-		    {
-		        case LISTEN:
-		        servers[serverIndex].setListen(strtoi(value), servers);
-				break;
+        if (getline(ss, key, '=') && getline(ss, value) && serverIndex >= 0 && serverIndex < numServers) {
+            switch (getKeyIndex(trim(key))) {
+                case LISTEN:
+                    servers[serverIndex].setListen(strtoi(value));
+                    break;
 
-				case SERVER_NAME:
-				servers[serverIndex].setServerName(value);
-                break;
+                case SERVER_NAME:
+                    servers[serverIndex].setServerName(value);
+                    break;
 
                 case HOST:
-                servers[serverIndex].setHost(value);
-                break;
+                    servers[serverIndex].setHost(value);
+                    break;
 
                 case ROOT:
-                servers[serverIndex]._root = setRoot(trim(value));
-                break;
-      
+                    servers[serverIndex]._root = setRoot(trim(value));
+                    break;
+
                 case CLIENT_MBSIZE:
-                servers[serverIndex].setBodySize(trim(value));
-                break;
-		    }
-		}
+                    servers[serverIndex].setBodySize(trim(value));
+                    break;
+            }
+        }
         else if (lines[i].substr(0, 5) == "route")
-           servers[serverIndex].setRoute(lines, i);
+            servers[serverIndex].setRoute(lines, i);
+        else if (lines[i] == "}")
+            break;
     }
     return (servers);
 }
 
 static int getKeyIndex(std::string key)
 {
-	std::string types[] = {"listen", "server_name", "host", "root", "client_max_body_size"};
+    std::string types[] = {"listen", "server_name", "host", "root", "client_max_body_size"};
 
-	for (size_t i = 0; i < 6; i++)
-		if (key == types[i])
-			return (i);
-	return (-1);
+    for (size_t i = 0; i < 6; i++)
+        if (key == types[i])
+            return (i);
+    return (-1);
 }
