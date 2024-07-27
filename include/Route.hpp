2d279ecc21990d6a7772f9e7c5190fe88d5da8b7
Renan Afonso Batista <rbatista@resutec.com.br>
2024-07-27 17:02:54 -0300

    feat: separado route de server e migrado a logica propria para melhorar a organizacao do codigo. todo: ainda falta tratar a adição de routes e testar tudo.

---
+++ include/Route.hpp
@@ -0,0 +1,53 @@
+#ifndef ROUTE_HPP
+#define ROUTE_HPP
+
+#include "defines.hpp"
+
+class Route {
+    private:
+        string _route;
+        bool _autoIndex;
+        bool _cgiOn;
+        string _root;
+        string _allowMethods;
+        string _index;
+        string _redirect;
+        string _return;
+        string _cgi;
+
+    public:
+        Route();
+        ~Route();
+
+        bool getAutoIndex() const;
+        bool getCgiOn() const;
+        string getRoot() const;
+        string getAllowMethods() const;
+        string getIndex() const;
+        string getRedirect() const;
+        string getReturn() const;
+        string getCgi() const;
+        string getRoute() const;
+
+        void setRoute(const string& route);
+        void setAutoIndex(bool autoIndex);
+        void setCgiOn(bool cgiOn);
+        void setRoot(string& root);
+        void setAllowMethods(const string& allowMethods);
+        void setIndex(string& index);
+        void setRedirect(const string& redirect);
+        void setReturn(const string& ret);
+        void setCgi(string& cgi);
+
+        class exception : public std::exception {
+            private:
+                string msg;
+
+            public:
+                exception(const string& msg);
+                virtual ~exception() throw();
+                virtual const char* what() const throw();
+        };
+};
+
+#endif  // ROUTE_HPP
---
+++ src/Route.cpp
@@ -0,0 +1,137 @@
+#include "Route.hpp"
+
+Route::Route()
+    : _autoIndex(false)
+    , _cgiOn(false)
+    , _route("")
+    , _root("/data/")
+    , _allowMethods("GET POST DELETE")
+    , _index("index.html")
+    , _redirect("")
+    , _return("")
+    , _cgi("")
+{
+}
+
+Route::~Route() {}
+
+string Route::getRoute() const
+{
+    return _route;
+}
+
+bool Route::getAutoIndex() const
+{
+    return _autoIndex;
+}
+
+bool Route::getCgiOn() const
+{
+    return _cgiOn;
+}
+
+string Route::getRoot() const
+{
+    return _root;
+}
+
+string Route::getAllowMethods() const
+{
+    return _allowMethods;
+}
+
+string Route::getIndex() const
+{
+    return _index;
+}
+
+string Route::getRedirect() const
+{
+    return _redirect;
+}
+
+string Route::getReturn() const
+{
+    return _return;
+}
+
+string Route::getCgi() const
+{
+    return _cgi;
+}
+
+void Route::setRoute(const string& route)
+{
+    _route = route;
+}
+
+void Route::setAutoIndex(bool autoIndex)
+{
+    _autoIndex = autoIndex;
+}
+
+void Route::setCgiOn(bool cgiOn)
+{
+    _cgiOn = cgiOn;
+}
+
+void Route::setRoot(string& root)
+{
+    if (root[0] != '/')
+        throw Route::exception(RED "Error: misformatted root path, please use '/path'" RESET);
+    if (root.substr(root.length() - 1) != "/")
+        root.insert(root.end(), '/');
+    _root = root;
+}
+
+void Route::setAllowMethods(const string& allowMethods)
+{
+    string allow_method;
+    stringstream ss(allowMethods);
+
+    while (ss >> allow_method) {
+        if (allow_method == "get" || allow_method == "GET")
+            continue;
+
+        else if (allow_method == "post" || allow_method == "POST")
+            continue;
+
+        else if (allow_method == "delete" || allow_method == "DELETE")
+            continue;
+        else
+            throw invalid_argument(RED "Error: invalid method" RESET);
+    }
+    _allowMethods = allowMethods;
+}
+
+void Route::setIndex(string& index)
+{
+    if (index[0] == '/')
+        index.erase(index.begin());
+    if (index.substr(index.length() - 1) == "/")
+        index.erase(index.length() - 1);
+}
+
+void Route::setRedirect(const string& redirect)
+{
+    if (redirect[0] == '/' || redirect.empty())
+        throw Route::exception(RED "Error: this is not a valid url" RESET);
+    _redirect = redirect;
+}
+
+void Route::setReturn(const string& ret)
+{
+    _return = ret;
+}
+
+void Route::setCgi(string& cgi)
+{
+    if (cgi.substr(cgi.find_last_of(".") + 1) != ".py")
+        throw invalid_argument(RED "Error: file extension is not '.py'" RESET);
+
+    if (cgi[0] == '/')
+        cgi.erase(cgi.begin());
+    if (cgi.substr(cgi.length() - 1) == "/")
+        cgi.erase(cgi.length() - 1);
+    _cgi = cgi;
+}
