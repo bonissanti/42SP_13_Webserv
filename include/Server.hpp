e9dcb5da04c1d211992f41ba755a0a75da51d177
Renan Afonso Batista <rbatista@resutec.com.br>
2024-07-27 16:59:45 -0300

    fix: unido setters e server, retirado struct para route e adicionado vetor de route.

--- include/Server.hpp
+++ include/Server.hpp
@@ -3,8 +3,9 @@
 
 #include "Config.hpp"
 #include "Request.hpp"
-#include "defines.hpp"
 #include "Utils.hpp"
+#include "defines.hpp"
+#include "Route.hpp"
 
 typedef enum {
     DEFAULT = 0,
@@ -18,55 +19,40 @@ typedef enum {
     MOVED_PERMANENTLY = 301,
 } status_request;
 
-typedef struct s_route {
-        bool _autoIndex;
-        bool _cgiOn;
-        std::string _route;
-        std::string _root;
-        std::string _allowMethods;
-        std::string _index;
-        std::string _redirect;
-        std::string _return;
-        std::string _cgi;
-} t_route;
-
 class Server {
     private:
         int _listen;
-        std::string _server_name;
-        std::string _host;  // acredito que seja o mesmo que _server_name
-        std::string _root;
+        string _server_name;
+        string _root;
         int _client_max_body_size;
-        std::vector<status_request> _error_page;
-        std::vector<t_route> _route;
+        vector<int> _error_page;
+        vector<Route> _routes;
 
     public:
         Server(void);
         ~Server();
 
-        t_route createRoute();
-        void setListen(int port);
-        void setServerName(std::string name);
-        void setHost(std::string host);
-        void setBodySize(std::string size);
-        void setRoute(std::vector<std::string> routeLines, size_t& i);
+        void create(ifstream& file);
+        static void startServer(vector<Server> servers);
 
-        static std::vector<Server> creatingServers(int numServers, std::vector<std::string> lines);
-        static void startServer(std::vector<Server> servers);
+        void setListen(int port);
+        void setServerName(string name);
+        void setHost(string host);
+        void setClientMaxBodySize(string size);
+        void setRoute(vector<string> routeLines, size_t& i);
+        void setErrorPage(string error_page);
 
         class exception : public std::exception {
             private:
-                std::string msg;
+                string msg;
 
             public:
-                exception(const std::string& msg);
+                exception(const string& msg);
                 virtual ~exception() throw();
                 virtual const char* what() const throw();
         };
 };
 
-// void start(); // void loadConfig(const std::string& config_path);
-// void handleClient(int client_socket);
-std::string setRoot(std::string root);
+string setRoot(string root);
 
 #endif  // SERVER_HPP
