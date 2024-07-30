4fb97eb015c77844abde574395857a3a7cdbc737
Renan Afonso Batista <rbatista@resutec.com.br>
2024-07-30 18:45:12 -0300

    fix: tentativa de correção do main apos merge.

--- src/main.cpp
+++ src/main.cpp
@@ -9,21 +9,7 @@ int main(int argc, char** argv)
 
         int numbersOfServers = Utils::getServersNumber(argv[1]);
         Server servers[numbersOfServers];
-        for(int i = 0; i < numbersOfServers; i++)
-        {
-            servers[i].
-        }
-
-        // Validate request(argv[1]);
-        // std::vector<Server> servers = Server::creatingServers(request.getNumServers(), request.getLines());
-
-        Server::startServer(servers);
-        Server::setupPolls(servers);
-    }
-    catch (const Server::exception& e) {
-        std::cerr << e.what() << '\n';
-        vector<Server> servers(numbersOfServers);
-
+   
         ifstream file(argv[1]);
         if (!file.is_open()) {
             return -1;
@@ -32,6 +18,13 @@ int main(int argc, char** argv)
         for (int i = 0; i < numbersOfServers; i++) {
             servers[i].create(file);
         }
+
+        Server::startServer(servers);
+        Server::setupPolls(servers);
+    }
+    catch (const Server::exception& e) {
+        std::cerr << e.what() << '\n';
+
     }
     catch (const Server::exception& e) {
         cerr << e.what() << '\n';
