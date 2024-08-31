7d3ea2c550e750ac8bdaaca86f007190fce44d43
Renan Afonso Batista <rbatista@resutec.com.br>
2024-08-30 21:59:03 -0300

    fix: refatorado cliente para servir de ponte para outras classes.

--- src/class/Client/Client.cpp
+++ src/class/Client/Client.cpp
@@ -1,8 +1,18 @@
 #include "Client.hpp"
 
-Client::Client() {}
+Client::Client(){}
 
-Client::~Client() {}
+Client::Client(Server* server, Request* request)
+{
+    _server = new Server(*server);
+    _request = new Request(*request);
+    _response = NULL;
+}
+
+Client::~Client() {
+    delete _server;
+    delete _request;
+}
 
 Client::ClientException::ClientException(const string& message) : msg(message) {}
 Client::ClientException::~ClientException() throw() {}
@@ -10,22 +20,6 @@ Client::ClientException::~ClientException() throw() {}
 const char* Client::ClientException::what() const throw()
 {
     return (msg.c_str());
-}
-
-void Client::addAssociation(int clientFd, Server server)
-{
-    _fdsMap[clientFd] = server;
-}
-
-Server Client::getServerFd(int clientFd)
-{
-    Server null;
-    if (_fdsMap.find(clientFd) != _fdsMap.end())
-        return (_fdsMap[clientFd]);
-    else {
-        cerr << RED << "Error: serverFd not found for request" << RESET;
-    }
-    return (null);
 }
 
 int Client::getMethodIndex(string method)
@@ -38,77 +32,91 @@ int Client::getMethodIndex(string method)
     return (-1);
 }
 
+Server* Client::getServer(void)
+{
+    return (_server);
+}
+
+Request* Client::getRequest(void)
+{
+    return (_request);
+}
+
+Response* Client::getResponse(void)
+{
+    return (_response);
+}
+
 int Client::callMethod()
 {
-    if (_request.getURI().empty())
-        return (_request.setStatusCode(BAD_REQUEST));
+    if (_request->getURI().empty())
+        return (_request->setStatusCode(BAD_REQUEST));
 
-    switch (getMethodIndex(_request.getMethod())) {
+    switch (getMethodIndex(_request->getMethod())) {
         case GET:
-           return runGetMethod();
+            return runGetMethod();
         case POST:
-           return runPostMethod();
+            return runPostMethod();
         case DELETE:
-           return runDeleteMethod();
+            return runDeleteMethod();
         default:
-            return (_request.setStatusCode(NOT_FOUND));
+            return (_request->setStatusCode(NOT_FOUND));
     }
 }
 
 int Client::runDeleteMethod()
 {
-    string uri = _request.getURI();
+    string uri = _request->getURI();
     string filePath = defineFilePath();
 
     if (!Utils::fileExists(filePath)) {
-        _request.setStatusCode(NOT_FOUND);
+        _request->setStatusCode(NOT_FOUND);
         return NOT_FOUND;
     }
 
     if (!Utils::hasDeletePermission(filePath)) {
-        _request.setStatusCode(FORBIDDEN);
+        _request->setStatusCode(FORBIDDEN);
         return FORBIDDEN;
     }
 
     if (remove(filePath.c_str()) != 0) {
-        _request.setStatusCode(INTERNAL_SERVER_ERROR);
+        _request->setStatusCode(INTERNAL_SERVER_ERROR);
         return INTERNAL_SERVER_ERROR;
     }
 
-    _response.setStatusCode(OK);
+    _response->setStatusCode(OK);
     return OK;
 }
 
 int Client::runGetMethod()
 {
-    string uri = _request.getURI();
+    string uri = _request->getURI();
     string filePath = defineFilePath();
     string contentType = defineContentType(filePath);
-    string responseBody = defineResponseBody(filePath, _request);
+    string responseBody = defineResponseBody(filePath);
     string contentLength = defineContentLength(responseBody);
 
-    _response.setFilePath(filePath);
-    _response.setContentType(contentType);
-    _response.setResponseBody(responseBody);
-    _response.setContentLength(contentLength);
-    _response.setStatusCode(OK);// TODO: colocar o status code correto conforme o ocorrido
+    _response->setFilePath(filePath);
+    _response->setContentType(contentType);
+    _response->setResponseBody(responseBody);
+    _response->setContentLength(contentLength);
+    _response->setStatusCode(OK);  // TODO: colocar o status code correto conforme o ocorrido
     return (OK);
 }
-
 
 string Client::defineFilePath(void)
 {
     string filePath;
 
-    if (_request.getURI() == "/") {
+    if (_request->getURI() == "/") {
         filePath = "content/index.html";
     }
     else {
-        filePath = "content" + _request.getURI(); // TODO: nem sempre a pasta sera a content, precisa ler e pegar corretamente a pasta conforme a rota
+        filePath = "content" + _request->getURI();  // TODO: nem sempre a pasta sera a content, precisa ler e pegar
+                                                    // corretamente a pasta conforme a rota
     }
     return (filePath);
 }
-
 
 string Client::defineContentType(string filePath)
 {
@@ -136,57 +144,56 @@ string Client::defineContentType(string filePath)
     return ("text/plain;charset=UTF-8");
 }
 
-void Client::sendResponse(struct pollfd& pollFds, map<int, Request>& requests)
+void Client::sendResponse(void)
 {
-    Client::_request = requests[pollFds.fd];
     string build;
 
-    if (_request.getStatusCode() == BAD_REQUEST) {
-        build = _response.buildMessage();
-    } else {
-        callMethod();
-        build = _response.buildMessage();
+    if (_request->getStatusCode() == BAD_REQUEST) {
+        build = _response->buildMessage();
     }
-    send(pollFds.fd, build.c_str(), build.size(), 0);
+    else {
+        callMethod();
+        build = _response->buildMessage();
+    }
+    send(_server->getPollFd().fd, build.c_str(), build.size(), 0);
     cout << "Message sent" << endl;
 
-    requests.erase(pollFds.fd);
-    close(pollFds.fd);
+    close(_server->getPollFd().fd);
 
-    _request.clear();
-    _response.clear();
+    _request->clear();
+    _response->clear();
 }
 
-string Client::defineResponseBody(const string &filePath, Request &req)
+string Client::defineResponseBody(const string& filePath)
 {
-    if (_request.getIsCgi()) {
-        _response.getIndex() = req.getServer().getRoute()[0].getIndex();
+    if (_request->getIsCgi()) {
+        _response->getIndex() = _request->getServer().getRoute()[0].getIndex();
 
-        if (_response.getIndex().find(".py") != string::npos || _response.getIndex().find(".php") != string::npos)
-            return (_response.executeCGI(req));
+        if (_response->getIndex().find(".py") != string::npos || _response->getIndex().find(".php") != string::npos)
+            return (_response->executeCGI(*_request));
     }
 
     ifstream file(filePath.c_str());
     if (!file.is_open()) {
-        _response.setStatusCode(NOT_FOUND);
+        _response->setStatusCode(NOT_FOUND);
         return ("");
     }
     else if (file.fail()) {
-        _response.setStatusCode(INTERNAL_SERVER_ERROR);
+        _response->setStatusCode(INTERNAL_SERVER_ERROR);
         return ("");
     }
     stringstream buffer;
     buffer << file.rdbuf();
 
     if (!verifyPermission(filePath)) {
-        _response.setStatusCode(FORBIDDEN);
+        _response->setStatusCode(FORBIDDEN);
         return ("");
     }
     file.close();
     return (buffer.str());
 }
 
-bool Client::verifyPermission(const string &file)
+bool Client::verifyPermission(const string& file)
 {
     if (access(file.c_str(), F_OK) != 0)
         return (false);
@@ -195,7 +202,7 @@ bool Client::verifyPermission(const string &file)
     return (true);
 }
 
-string Client::defineContentLength(const string &body)
+string Client::defineContentLength(const string& body)
 {
     ostringstream oss;
 
--- src/class/Client/Client.hpp
+++ src/class/Client/Client.hpp
@@ -3,44 +3,47 @@
 
 #include <exception>
 
-#include "../Utils/Utils.hpp"
 #include "../Request/Request.hpp"
 #include "../Response/Response.hpp"
 #include "../Server/Server.hpp"
+#include "../Utils/Utils.hpp"
 
 class Client {
     private:
-        int _clientFd;
-        Server _server;
-        Response _response;
-        Request _request;
+        Server *_server;
+        Response *_response;
+        Request *_request;
 
         string defineFilePath(void);
         string defineContentType(string filePath);
-        string defineResponseBody(const string &filePath, Request &req);
+        string defineResponseBody(const string &filePath);
         bool verifyPermission(const string &file);
         string defineContentLength(const string &body);
+
     public:
         Client();
+        Client(Server *server, Request *request);
         ~Client();
         void addAssociation(int clientFd, Server server);
-        Server getServerFd(int clientFd);
         int callMethod(void);
-        int getMethodIndex(string method);
         int runGetMethod(void);
         int runPostMethod(void);
         int runDeleteMethod(void);
-        bool saveUploadedFile(const string& filename, const string& fileContent, const string& directory);
-        void sendResponse(struct pollfd& pollFds, map<int, Request>& requests);
+        bool saveUploadedFile(const string &filename, const string &fileContent, const string &directory);
+        void sendResponse(void);
+        int getMethodIndex(string method);
+        Server* getServer(void);
+        Response *getResponse(void);
+        Request *getRequest(void);
 
         class ClientException : public std::exception {
             private:
                 string msg;
 
             public:
-                ClientException(const string& message);
+                ClientException(const string &message);
                 virtual ~ClientException() throw();
-                virtual const char* what() const throw();
+                virtual const char *what() const throw();
         };
 };
 
