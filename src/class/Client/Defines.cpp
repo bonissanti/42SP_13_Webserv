bdbf3b7847ebb9c40d91ed421bb144fbec1f7c9f
Renan Batista <rbatista@resutec.com.br>
2024-09-19 23:47:20 +0000

    fix: correção de rotas.

--- src/class/Client/Defines.cpp
+++ src/class/Client/Defines.cpp
@@ -2,118 +2,95 @@
 
 string Client::defineFilePath(Route& route, string uri)
 {
-    string root = route.getRoot();
     string index = route.getIndex();
-    string routePath = route.getRoute();
-
-    if (uri.find(routePath) == 0 && routePath.length() > 1)
-        uri = uri.substr(routePath.length());
-    if (root[root.length() - 1] != '/')
-        root += "/";
-    if (!uri.empty() && uri[0] == '/')
-        uri = uri.substr(1);
-    string temp = root + uri, filePath;
-    
-    struct stat s;
-    if (stat(temp.c_str(), &s) == 0) {
-        if (s.st_mode & S_IFDIR) {
-            if (filePath[filePath.length() - 1] != '/')
-                filePath += "/";
-            if(!index.empty())
-                filePath += index;
+    string root = route.getRoot();
+    if (uri == "/") {
+        return (root.substr(0, root.length()) + '/' + index);
+    }
+    else if (count(uri.begin(), uri.end(), '/') == 1) {
+        if (uri.find('.') != string::npos) {
+            if (Utils::fileExists(root + '/' + uri))
+                return root.substr(0, root.length()) + '/' + uri;
+            else
+                return (root.substr(0, root.length()) + '/' + index);
         }
+        else
+            return (root.substr(0, root.length()) + '/' + index);
     }
-    else if (!Utils::fileExists(temp))
-    {
-        if(!index.empty())
-            filePath += root + index;
+    else if (count(uri.begin(), uri.end(), '/') > 1) {
+        string file = uri.substr(uri.find_last_of("/") + 1);
+        if (Utils::fileExists(root + '/' + file))
+            return root.substr(0, root.length()) + '/' + file;
+        else
+            return (root.substr(0, root.length()) + '/' + index);
     }
-    else
-        filePath = temp;
-
-        // if (Utils::fileExists(filePath)) {
-        //     if (route.getIndex().find(".html") != string::npos || route.getIndex().find(".php") != string::npos || route.getIndex().find(".py") != string::npos) {
-        //         size_t found = 0;
-        //         if (route.getIndex().find(".html") != string::npos)
-        //             found = route.getIndex().find(".php");
-        //         else if (route.getIndex().find(".php") != string::npos)
-        //             found = route.getIndex().find(".php");
-        //         else if (route.getIndex().find(".py") != string::npos)
-        //             found = route.getIndex().find(".py");
-
-        //         if (found == 0)
-        //             return (executeCGI(_request, _server, "/cgi/index.php"));
-        //         size_t start = route.getIndex().rfind(' ', found);
-        //         string file = route.getIndex().substr(start + 1, found - start - 1);
-        //         return (executeCGI(_request, _server, file));
-        // }
-    return (filePath);
+    return ("NF");
 }
 
-    string Client::defineContentType(string filePath)
-    {
-        size_t index;
-        string extension;
-        _mimeTypes[".html"] = "text/html";
-        _mimeTypes[".css"] = "text/css";
-        _mimeTypes[".txt"] = "text/plain";
-        _mimeTypes[".png"] = "image/png";
-        _mimeTypes[".jpg"] = "image/jpg";
-        _mimeTypes[".gif"] = "image/gif";
-        _mimeTypes[".js"] = "application/js";
-        _mimeTypes[".pdf"] = "application/pdf";
+string Client::defineContentType(string filePath)
+{
+    size_t index;
+    string extension;
+    _mimeTypes[".html"] = "text/html";
+    _mimeTypes[".css"] = "text/css";
+    _mimeTypes[".txt"] = "text/plain";
+    _mimeTypes[".png"] = "image/png";
+    _mimeTypes[".jpg"] = "image/jpg";
+    _mimeTypes[".gif"] = "image/gif";
+    _mimeTypes[".js"] = "application/js";
+    _mimeTypes[".pdf"] = "application/pdf";
 
-        index = filePath.rfind('.');
-        if (index != string::npos) {
-            extension = filePath.substr(index);
-            map<string, string>::iterator it = _mimeTypes.begin();
+    index = filePath.rfind('.');
+    if (index != string::npos) {
+        extension = filePath.substr(index);
+        map<string, string>::iterator it = _mimeTypes.begin();
 
-            for (; it != _mimeTypes.end(); ++it)
-                if (it->first == extension)
-                    return (it->second + ";charset=UTF-8");
-        }
-        return ("text/html;charset=UTF-8");
+        for (; it != _mimeTypes.end(); ++it)
+            if (it->first == extension)
+                return (it->second + ";charset=UTF-8");
+    }
+    return ("text/html;charset=UTF-8");
+}
+
+string Client::defineResponseBody(const Route& route, const string& filePath)
+{
+    if (route.getCgiOn()) {
+        if (filePath.find(".py") != string::npos || filePath.find(".php") != string::npos)
+            return (executeCGI(_request, _server, filePath));
     }
 
-    string Client::defineResponseBody(const Route& route, const string& filePath)
-    {
-        if (route.getCgiOn()) {
-            if (filePath.find(".py") != string::npos || filePath.find(".php") != string::npos)
-                return (executeCGI(_request, _server, filePath));
-        }
-
-        struct stat path_stat;
-        stat(filePath.c_str(), &path_stat);
-        if (S_ISDIR(path_stat.st_mode)) {
-            if (_subdirAutoindex)
-                return (_response.handleAutoIndex(filePath, _request.getURI()));
-        }
-
-        ifstream file(filePath.c_str());
-        if (!file.is_open()) {
-            _response.setStatusCode(NOT_FOUND);
-            return ("");
-        }
-        else if (file.fail()) {
-            _response.setStatusCode(INTERNAL_SERVER_ERROR);
-            return ("");
-        }
-        stringstream buffer;
-        buffer << file.rdbuf();
-
-        if (!verifyPermission(filePath)) {
-            _response.setStatusCode(FORBIDDEN);
-            return ("");
-        }
-        file.close();
-        return (buffer.str());
+    struct stat path_stat;
+    stat(filePath.c_str(), &path_stat);
+    if (S_ISDIR(path_stat.st_mode)) {
+        if (_subdirAutoindex)
+            return (_response.handleAutoIndex(filePath, _request.getURI()));
     }
 
-    string Client::defineContentLength(const string& body)
-    {
-        ostringstream oss;
-
-        size_t len = body.size();
-        oss << len;
-        return (oss.str());
+    ifstream file(filePath.c_str());
+    if (!file.is_open()) {
+        _response.setStatusCode(NOT_FOUND);
+        return ("");
     }
+    else if (file.fail()) {
+        _response.setStatusCode(INTERNAL_SERVER_ERROR);
+        return ("");
+    }
+    stringstream buffer;
+    buffer << file.rdbuf();
+
+    if (!verifyPermission(filePath)) {
+        _response.setStatusCode(FORBIDDEN);
+        return ("");
+    }
+    file.close();
+    return (buffer.str());
+}
+
+string Client::defineContentLength(const string& body)
+{
+    ostringstream oss;
+
+    size_t len = body.size();
+    oss << len;
+    return (oss.str());
+}
